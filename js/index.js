ecb8d1732779055a05b3f885765d8fb0e1c6a7ec
azizqamar7 <azizqamar7@gmail.com>
2022-12-24 00:28:49 +0530

    Updated

--- js/index.js
+++ js/index.js
@@ -8,12 +8,12 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD7D0f7Qt9z5PVnEM1qdLhUZBgaMG3dlAk",
-    authDomain: "store-canvas-images.firebaseapp.com",
-    projectId: "store-canvas-images",
-    storageBucket: "store-canvas-images.appspot.com",
-    messagingSenderId: "877456558384",
-    appId: "1:877456558384:web:449a2e6c65d86b5b8420f5",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
     measurementId: "G-JD8J9MFHH9",
 };
 
